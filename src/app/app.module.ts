15e06e7d7d202ccf91314bdb4967d92c33e96849
miguel porras <porraaangel@gmail.com>
2023-04-04 08:33:59 -0500

    proyecto inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PokemonsComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatIconModule
+ 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
